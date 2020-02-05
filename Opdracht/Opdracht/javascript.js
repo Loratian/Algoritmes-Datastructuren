56262b08f08d2a8c5c2c4024092400d8fb78fb54
Muller <zoe.muller@capgemini.com>
2020-02-05 12:05:07 +0100

    US2

--- Opdracht/Opdracht/javascript.js
+++ Opdracht/Opdracht/javascript.js
@@ -11,7 +11,7 @@ function importAppointment() {
 	appointmentSort();
 	console.log(everyAppointment);
 	loadAppointments();
-	
+
 }
 
 const LOG_DETAILS = 1;
@@ -29,9 +29,9 @@ const LOG_LEVEL = LOG_DETAILS;
  * @param {*} msgSeverity (one of the values LOG_DETAILS, LOG_STEPS, LOG_ERRORS)
  */
 
-function logInfo (msg,msgSeverity) {
+function logInfo(msg, msgSeverity) {
 	if (msgSeverity >= LOG_LEVEL) {
-		console.log (msg);
+		console.log(msg);
 	}
 }
 
@@ -42,12 +42,12 @@ function logInfo (msg,msgSeverity) {
  * @param {*} fileName 
  */
 
-function importMap (fileName) {
+function importMap(fileName) {
 
- 	var xmlhttp = new XMLHttpRequest();
+	var xmlhttp = new XMLHttpRequest();
 	var url = fileName;
-	
-	xmlhttp.onreadystatechange = function() {
+
+	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
 			var myArr = JSON.parse(this.responseText);
 			processMapInfo(myArr);
@@ -61,14 +61,14 @@ function importMap (fileName) {
  * Reads the line information and updates the internal map structure
  * @param {*} lineData (JSON string as object)
  */
-function processMapInfo (lineData) {
+function processMapInfo(lineData) {
 	var mainItem;
 	var networkItem;
 	var networkItemStop;
 	var listOfStops = {}; // declared as associative array
 
 	for (mainItem in lineData) {
-		logInfo (mainItem,LOG_DETAILS);
+		logInfo(mainItem, LOG_DETAILS);
 		// There should be only 1 mainItem that starts with something like "GVB_"
 		// We need this to access the other information
 
@@ -83,13 +83,13 @@ function processMapInfo (lineData) {
 				// add a stop to a list based on its order; this is described by UserStopOrderNumber
 				// but only when it does not exist already
 				if (!listOfStops.hasOwnProperty(lineData[mainItem]["Network"][networkItem][networkItemStop].UserStopOrderNumber)) {
-					listOfStops [lineData[mainItem]["Network"][networkItem][networkItemStop].UserStopOrderNumber] = 
+					listOfStops[lineData[mainItem]["Network"][networkItem][networkItemStop].UserStopOrderNumber] =
 						lineData[mainItem]["Network"][networkItem][networkItemStop].TimingPointName;
 				}
 			}
 		}
-	}	
-	logInfo (listOfStops,LOG_DETAILS);
+	}
+	logInfo(listOfStops, LOG_DETAILS);
 
 	// listOfStops now has all the info to update your map structure :-)
 }
@@ -145,49 +145,50 @@ function processMapInfo (lineData) {
 // 	}
 
 // };
-	
+
 function appointmentSort() {
-    for(let i = everyAppointment.length - 1; i > 0; i--) {
-        for(let j = 0; j < i; j++) {
-            let date1 = new Date(everyAppointment[j].Afspraak.gewenstTijdstip);
-            let date2 = new Date(everyAppointment[j+1].Afspraak.gewenstTijdstip);
-            if(date1.getTime() > date2.getTime()) {
-              let temp = everyAppointment[j];
-              everyAppointment[j] = everyAppointment[j+1];
-              everyAppointment[j+1] = temp;
-            }
-        }
-    }
+	for (let i = everyAppointment.length - 1; i > 0; i--) {
+		for (let j = 0; j < i; j++) {
+			let date1 = new Date(everyAppointment[j].Afspraak.gewenstTijdstip);
+			let date2 = new Date(everyAppointment[j + 1].Afspraak.gewenstTijdstip);
+			if (date1.getTime() > date2.getTime()) {
+				let temp = everyAppointment[j];
+				everyAppointment[j] = everyAppointment[j + 1];
+				everyAppointment[j + 1] = temp;
+			}
+		}
+	}
 }
 
 function loadAppointments() {
-		document.getElementById("appointmentContainer").innerHTML = "";
-  		var list = document.createElement('ul');
+	document.getElementById("appointmentContainer").innerHTML = "";
+	var list = document.createElement('ul');
 
-		everyAppointment.forEach(function (appointment) {
-    	var li = document.createElement('li');
-		li.textContent = appointment.Afspraak.Id + "  |  " + appointment.Afspraak.naamKlant + "  |  "  + " woont op " + appointment.Afspraak.adresKlant + "  |  " + " en wil een afspraak op: " + appointment.Afspraak.gewenstTijdstip + "  |  " + ". De klant heeft als dichtsbijzijnde halte de halte in de " + appointment.Afspraak.dichtsbijzijndeHalte + "  |  " + ". De reden voor deze afspraak is: " + appointment.Afspraak.redenAfspraak + "  |  " + appointment.Afspraak.naamMonteur;
+	everyAppointment.forEach(function (appointment) {
+		var li = document.createElement('li');
+		li.textContent = appointment.Afspraak.Id + "  |  " + appointment.Afspraak.naamKlant + "  |  " + " woont op " + appointment.Afspraak.adresKlant + "  |  " + " en wil een afspraak op: " + appointment.Afspraak.gewenstTijdstip + "  |  " + ". De klant heeft als dichtsbijzijnde halte de halte in de " + appointment.Afspraak.dichtsbijzijndeHalte + "  |  " + ". De reden voor deze afspraak is: " + appointment.Afspraak.redenAfspraak + "  |  " + appointment.Afspraak.naamMonteur;
 		list.appendChild(li);
-		});
+	});
 
-		var app = document.querySelector('#appointmentContainer');
-		app.appendChild(list);
-		
+	var app = document.querySelector('#appointmentContainer');
+	app.appendChild(list);
+
 };
-	
-	
-	
+function voegAfspraakToe() {
 
-
+}
 
 function addButtonActions() {
 	var afsprakenLaden = document.getElementById("afspraakContent");
-	
-	afsprakenLaden.addEventListener('click', function() {
-	importAppointment();
-	});
-}
+	var afsprakenToevoegen = document.getElementById("afspraakToeContainer");
 
+	afsprakenLaden.addEventListener('click', function () {
+		laadAfspraken();
+	}),
+		afsprakenToevoegen.addEventListener('click', function () {
+			voegAfspraakToe();
+		});
+}
 
 
 /**
@@ -195,27 +196,27 @@ function addButtonActions() {
  * Create the map of tram and metro data
  */
 
-importMap ("https://ori.clow.nl/algds/GVB_1_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_2_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_3_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_4_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_5_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_7_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_11_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_12_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_13_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_14_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_17_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_19_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_24_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_26_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_50_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_51_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_52_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_53_1.json");
-importMap ("https://ori.clow.nl/algds/GVB_54_1.json");
+importMap("https://ori.clow.nl/algds/GVB_1_1.json");
+importMap("https://ori.clow.nl/algds/GVB_2_1.json");
+importMap("https://ori.clow.nl/algds/GVB_3_1.json");
+importMap("https://ori.clow.nl/algds/GVB_4_1.json");
+importMap("https://ori.clow.nl/algds/GVB_5_1.json");
+importMap("https://ori.clow.nl/algds/GVB_7_1.json");
+importMap("https://ori.clow.nl/algds/GVB_11_1.json");
+importMap("https://ori.clow.nl/algds/GVB_12_1.json");
+importMap("https://ori.clow.nl/algds/GVB_13_1.json");
+importMap("https://ori.clow.nl/algds/GVB_14_1.json");
+importMap("https://ori.clow.nl/algds/GVB_17_1.json");
+importMap("https://ori.clow.nl/algds/GVB_19_1.json");
+importMap("https://ori.clow.nl/algds/GVB_24_1.json");
+importMap("https://ori.clow.nl/algds/GVB_26_1.json");
+importMap("https://ori.clow.nl/algds/GVB_50_1.json");
+importMap("https://ori.clow.nl/algds/GVB_51_1.json");
+importMap("https://ori.clow.nl/algds/GVB_52_1.json");
+importMap("https://ori.clow.nl/algds/GVB_53_1.json");
+importMap("https://ori.clow.nl/algds/GVB_54_1.json");
 
-addButtonActions();   
+addButtonActions();
 // add a short wait so we know ALL 16 lines have been processed
 
 
